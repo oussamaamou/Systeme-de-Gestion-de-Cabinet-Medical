de8b1e8b4ab86d5b4320fbcd48f9a205663427d4
oussamaamou <amoussdark@gmail.com>
2025-02-08 15:31:45 +0100

    Fichier de creation des routes

--- config/routes.php
+++ config/routes.php
@@ -1,7 +1,24 @@
 <?php
+namespace Config;
 
 use Core\Router;
 
-Router::add("/", "AuthController", "index");
-Router::add("/login", "AuthController", "login");
-Router::add("/inscription", "AuthController", "register");
+class Routes
+{
+    public static function registerRoutes(Router $router)
+    {
+        $router->get('/log', 'AuthControllers@loginForm');
+        $router->get('/inscription', 'AuthControllers@inscriptionForm');
+        $router->post('/inscription', 'AuthControllers@inscription');
+        $router->post('/login', 'AuthControllers@login');
+        $router->get('/logout', 'AuthControllers@logout');
+        $router->get('/patients', 'PatientControllers@index');
+        $router->get('/reservations', 'PatientControllers@reservations');
+        $router->get('/consultations', 'PatientControllers@consultations');
+        $router->get('/reservations', 'PatientControllers@listeconsultations');
+        $router->get('/medecins', 'MedecinControllers@index');
+        $router->get('/rendezvous', 'RendezVousControllers@index');
+        $router->post('/rendezvous/create', 'RendezVousControllers@create');
+    }
+}
+?>
