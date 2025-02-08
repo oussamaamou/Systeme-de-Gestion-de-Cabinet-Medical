1182817f74871df5950ffc4178343f16da7ed6b7
oussamaamou <amoussdark@gmail.com>
2025-02-08 15:32:37 +0100

    Fichier de manipulation des views

---
+++ core/Controller.php
@@ -0,0 +1,12 @@
+<?php
+namespace Core;
+
+class Controller
+{
+    protected function view($view, $data = [])
+    {
+        extract($data);
+        require __DIR__ . '/../app/views/' . $view . '.php';
+    }
+}
+?>
