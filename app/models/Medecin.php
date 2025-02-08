f7dda01fa5a322e56c37181fc3cb0a5c53230015
oussamaamou <amoussdark@gmail.com>
2025-02-08 15:24:24 +0100

    Creation du model Medecin

---
+++ app/models/Medecin.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Models;
+
+use Core\Database;
+use PDO;
+
+class Medecin
+{
+    public static function all()
+    {
+        $db = Database::getConnection();
+        $stmt = $db->prepare("SELECT Nom, Prenom, Email, Telephone FROM utilisateurs WHERE Role = 'Medecin'");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+?>
