f551d6e0901699b34c5ef42638371312977b1456
oussamaamou <amoussdark@gmail.com>
2025-02-08 15:27:52 +0100

    Ajout de la page d'inscription

--- app/views/auth/inscription.php
+++ app/views/auth/inscription.php
@@ -15,14 +15,14 @@
 
     <main>
         
-        <section class="bg-green-200 py-[5rem]" style="background-image: url(); background-repeat: no-repeat; background-size: cover;">
+        <section class="bg-teal-200 py-[5rem]" style="background-image: url(); background-repeat: no-repeat; background-size: cover;">
             <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         Créer votre compte
                         </h1>
-                        <form class="space-y-4 md:space-y-6" action="inscription.php" method="POST" >
+                        <form class="space-y-4 md:space-y-6" action="inscription" method="POST" >
                             <div>
                                 <label for="nom" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom</label>
                                 <input type="text" name="nom" id="nom" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ahmed" required="">
@@ -50,9 +50,9 @@
                                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                                 <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                             </div>
-                            <button type="submit" class="w-full text-white bg-green-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">S'inscrire</button>
+                            <button type="submit" class="w-full text-white bg-teal-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">S'inscrire</button>
                             <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Vous avez déjà un compte? <a href="login.php" class="font-medium text-stone-700 hover:underline hover:text-green-500 dark:text-primary-500">Se connecter</a>
+                                Vous avez déjà un compte? <a href="log" class="font-medium text-stone-700 hover:underline hover:text-teal-500 dark:text-primary-500">Se connecter</a>
                             </p>
                         </form>
                     </div>
@@ -103,3 +103,5 @@
 
 </body>
 </html>
+
+
