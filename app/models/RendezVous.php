35b2532ffda906b5e2160acacb29034ff02cfc82
oussamaamou <amoussdark@gmail.com>
2025-02-10 23:59:41 +0100

    Ajout des fonctions du modele rendez-vous

--- app/models/RendezVous.php
+++ app/models/RendezVous.php
@@ -4,6 +4,37 @@ namespace App\Models;
 use Core\Database;
 
 class RendezVous{
+
+    public function getMyReservation($ID_patient) {
+        $db = Database::getConnection();
+
+        $stmt = $db->prepare("SELECT rendezvous.ID, rendezvous.Patient_id, rendezvous.Medecin_id, rendezvous.Date_reservation, 
+                                     rendezvous.Statut, utilisateurs.Nom, utilisateurs.Prenom, utilisateurs.Email
+                              FROM rendezvous 
+                              JOIN utilisateurs ON utilisateurs.ID = rendezvous.Medecin_id
+                              WHERE rendezvous.Patient_id = :ID_patient AND Statut = 'en attente'");
+                              
+        $stmt->bindParam(':ID_patient', $ID_patient, \PDO::PARAM_INT);
+        $stmt->execute();
+        
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function getMyConsultations($ID_medecin) {
+        $db = Database::getConnection();
+
+        $stmt = $db->prepare("SELECT rendezvous.ID, rendezvous.Patient_id, rendezvous.Medecin_id, rendezvous.Date_reservation, 
+                                     rendezvous.Statut, utilisateurs.Nom, utilisateurs.Prenom, utilisateurs.Email
+                              FROM rendezvous 
+                              JOIN utilisateurs ON utilisateurs.ID = rendezvous.Patient_id
+                              WHERE rendezvous.Medecin_id = :ID_medecin AND Statut = 'en attente'");
+                              
+        $stmt->bindParam(':ID_medecin', $ID_medecin, \PDO::PARAM_INT);
+        $stmt->execute();
+        
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+    
     
    
 }
